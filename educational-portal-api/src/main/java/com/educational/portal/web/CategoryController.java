e762608b74ae1373b263199b39cdca82a8dd03e8
Roman Koryshko <korishgaming@gmail.com>
2022-11-29 22:22:45 +0100

    Implement additional Categories endpoints

--- educational-portal-api/src/main/java/com/educational/portal/web/CategoryController.java
+++ educational-portal-api/src/main/java/com/educational/portal/web/CategoryController.java
@@ -4,11 +4,13 @@ import com.educational.portal.domain.dto.CategoryDto;
 import com.educational.portal.domain.dto.CreateCategoryRequest;
 import com.educational.portal.service.CategoryService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -28,8 +30,9 @@ public class CategoryController {
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<CategoryDto> findById(@PathVariable Long id) {
-		CategoryDto category = categoryService.findById(id);
+	public ResponseEntity<CategoryDto> findById(@PathVariable Long id,
+												@RequestParam(name = "isHideSubCategories", required = false ) boolean isHideSubCategories) {
+		CategoryDto category = categoryService.findByIdAndConvertToDto(id, isHideSubCategories);
 		return ResponseEntity.ok(category);
 	}
 
@@ -50,4 +53,17 @@ public class CategoryController {
 
 		return ResponseEntity.created(location).build();
 	}
+	@PostMapping("/{categoryId}/assignSubCategory/{subCategoryId}")
+	public ResponseEntity<?> assignSubcategoryToCategory(@PathVariable(name = "categoryId") Long categoryId,
+														 @PathVariable(name = "subCategoryId") Long subCategoryId ) {
+		categoryService.assignSubcategoryToCategory(categoryId, subCategoryId);
+		return ResponseEntity.ok("");
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteCategory(@PathVariable Long id) {
+		categoryService.deleteCategoryById(id);
+		return ResponseEntity.ok("category with this id " + id + " and their subcategories is deleted");
+	}
+
 }
